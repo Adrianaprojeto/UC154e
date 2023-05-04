59e010370a8532b9746037c6752fc87c19d09889
Adrianaok <adrianabeco203@gmail.com>
2023-05-03 21:39:40 -0300

    segundo commit

---
+++ TesteIMC/TestarIMC/CalculoIMCTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools;
+using TesteIMC;
+
+
+namespace TestarIMC
+{
+    //metodo de testes que voce quer executar
+    [TestClass]
+    public class CalculoIMCTests
+        
+    {
+        //metodo de teste
+        [TestMethod]
+        //descrição do teste
+
+        public void CalcularIMC()
+        {
+            // arrange - organizar, preparar o teste
+            double peso = 110;
+            double altura = 1.79;
+
+            // act - agir - execução / chamada do metodo
+            var resultadoIMC = Operacoes.CalcularIMC(peso, altura);
+
+            // assert - comportamento esperado, comportamento obtido
+            Assert.AreEqual(34,33,resultadoIMC);
+        }
+
+    }
+}
---
+++ TesteIMC/TestarIMC/ClassificaIMCTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteIMC;
+
+namespace TestarIMC
+{
+    [TestClass]
+    public class ClassificaIMCTests
+    {
+        [TestMethod]
+
+        public void ClassificarIMC()
+        {
+            //Arrange
+            double imc = 29;
+            //act
+            var classificacaoIMC = Operacoes.ClassificarIMC(imc);
+            // Assert
+            Assert.AreEqual("Sobrepeso", classificacaoIMC);
+
+        }
+    }
+}
